00be7f3a11fc7af90b9e6bf19f403311b4392668
Vitali Adamenka <vitali_adamenka@epam.com>
2016-02-01 18:10:55 +0300

    Add true kibana run script.

--- start-kibana.sh
+++ start-kibana.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-ES_HOST=${ES_HOST:-pss-db}
+ES_HOST=${ES_HOST:-172.17.0.2}
 ES_PORT=${ES_PORT:-9200}
 ES_SCHEME=${ES_SCHEME:-http}
 KIBANA_CONFIG_PATH=/home/kibana-$KIBANA_VERSION/config/kibana.yml
 #Update kibana config
-sed -i '/elasticsearch_url:/c\elasticsearch_url: "'$ES_SCHEME'://'$ES_HOST':'$ES_PORT'"' ${KIBANA_CONFIG_PATH}
-
+sed -i 's@\#.elasticsearch.url:..http:\/\/localhost:9200.@elasticsearch_url: "'$ES_SCHEME'://'$ES_HOST':'$ES_PORT'"@' ${KIBANA_CONFIG_PATH}
 #Start Kibana
 /home/kibana-$KIBANA_VERSION/bin/kibana
